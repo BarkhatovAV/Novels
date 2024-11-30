ecae3e80757b826e7b01d519405108b86dc2525e
BarkhatovAV <barkhatov.a.v@mail.ru>
2024-11-30 16:42:38 +0300

    Update

--- Assets/Sources/Scripts/AdManager.cs
+++ Assets/Sources/Scripts/AdManager.cs
@@ -1,62 +1,54 @@
+using GamePush;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using GamePush;
-using VNCreator;
 
+public class AdManager : MonoBehaviour
+{
 
-    public class AdManager : MonoBehaviour
+    private bool _isFullScreenAvailable = true;
+    private float _time = 120;
+
+    public void ShowInterstitialAd(Action onShowCallback = null, Action<bool> onCloseCallback = null)
     {
-
-
-        private void OnHovered()
+        if (_isFullScreenAvailable == false)
         {
-
-        }
-
-        private bool _isFullScreenAvailable = true;
-        private float _time = 120;
-
-        public void ShowInterstitialAd(Action onShowCallback = null, Action<bool> onCloseCallback = null)
-        {
-            if (_isFullScreenAvailable == false)
-            {
-                onShowCallback?.Invoke();
-                onCloseCallback?.Invoke(true);
-                return;
-            }
-
-
-
-            //#if YANDEX_GAMES && !UNITY_EDITOR
-            //        if (YandexGamesSdk.IsInitialized)
-            //            Agava.YandexGames.InterstitialAd.Show(onShowCallback, onCloseCallback);
-            //#endif
-            GP_Ads.ShowFullscreen(onShowCallback, onCloseCallback);
-
-
-#if !UNITY_WEBGL || UNITY_EDITOR
             onShowCallback?.Invoke();
             onCloseCallback?.Invoke(true);
-#endif
+            return;
         }
 
-        public void ShowRewardAd(Action onOpenCallback = null, Action onRewardedCallback = null, Action onCloseCallback = null)
-        {
 
-            //#if YANDEX_GAMES && !UNITY_EDITOR
-            //        Agava.YandexGames.VideoAd.Show(onOpenCallback, onRewardedCallback, onCloseCallback);
-            //#endif
+
+        //#if YANDEX_GAMES && !UNITY_EDITOR
+        //        if (YandexGamesSdk.IsInitialized)
+        //            Agava.YandexGames.InterstitialAd.Show(onShowCallback, onCloseCallback);
+        //#endif
+        GP_Ads.ShowFullscreen(onShowCallback, onCloseCallback);
+
+
+#if !UNITY_WEBGL || UNITY_EDITOR
+        onShowCallback?.Invoke();
+        onCloseCallback?.Invoke(true);
+#endif
+    }
+
+    public void ShowRewardAd(Action onOpenCallback = null, Action onRewardedCallback = null, Action onCloseCallback = null)
+    {
+
+        //#if YANDEX_GAMES && !UNITY_EDITOR
+        //        Agava.YandexGames.VideoAd.Show(onOpenCallback, onRewardedCallback, onCloseCallback);
+        //#endif
 
 
 
 
 #if !UNITY_WEBGL || UNITY_EDITOR
-            onOpenCallback?.Invoke();
-            onCloseCallback?.Invoke();
+        onOpenCallback?.Invoke();
+        onCloseCallback?.Invoke();
 #endif
 
-        }
     }
 
+}
